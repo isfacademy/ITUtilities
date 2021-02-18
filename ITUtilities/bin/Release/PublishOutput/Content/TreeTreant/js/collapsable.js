05416e70663b9b13ebae636e8f0de8d766ef2182
isfacademy <isfacademy.2023@gmail.com>
2021-02-18 12:30:36 +0200

    c

---
+++ ITUtilities/bin/Release/PublishOutput/Content/TreeTreant/js/collapsable.js
@@ -0,0 +1,117 @@
+
+//// JSON approach
+//var chart_config = {
+//    chart: {
+//        container: "#collapsable-example",
+//        node: {
+//            HTMLclass: 'fancybox',
+//            collapsable: false
+//        },
+//        animateOnInit: true,
+//        animation: {
+//            nodeAnimation: "easeOutBounce",
+//            nodeSpeed: 700,
+//            connectorsAnimation: "bounce",
+//            connectorsSpeed: 700
+//        }
+//    },
+//    nodeStructure:
+//    {
+//        text: {
+//            name: "قائد المعهد",
+//            link: '#0'
+//        },
+//        children: [
+//            {
+//                text: {
+//                    name: "المساعد الثاني",
+//                    link: '#2',
+//                }
+//            },
+//            {
+//                text: {
+//                    name: "المساعد الأول",
+//                    link: '#3',
+//                },
+//                children: [
+//                    {
+//                        text: {
+//                            name: "فرع البريد",
+//                            link: '#4',
+//                        },
+//                    },
+//                    {
+//                        text: {
+//                            name: "فرع التدريب",
+//                            link: '#5',
+//                        },
+//                    },
+//                    {
+//                        pseudo: true,
+//                        children: [
+//                            {
+//                                text: {
+//                                    name: "قسم التدريب",
+//                                    link: '#6',
+//                                },
+//                            },
+//                            {
+//                                pseudo: true,
+//                                children: [
+//                                    {
+//                                        text: {
+//                                            name: "مدرسة المشتركة",
+//                                            link: '#7',
+//                                        },
+//                                    },
+//                                    {
+//                                        text: {
+//                                            name: "مدرسة الأفراد",
+//                                            link: '#8',
+//                                        },
+//                                    },
+//                                    {
+//                                        text: {
+//                                            name: "مدرسة الرتباء",
+//                                            link: '#9',
+//                                        },
+//                                    },
+//                                    {
+//                                        text: {
+//                                            name: "كلية الضباط",
+//                                            link: '#10',
+//                                        },
+//                                    }
+//                                ]
+//                            },
+//                            {
+//                                text: {
+//                                    name: "قسم الأبحاث والدروس",
+//                                    link: '#11',
+//                                },
+//                            }
+//                        ]
+//                    },
+//                    {
+//                        text: {
+//                            name: "فرع الإدارة",
+//                            link: '#12',
+//                        },
+//                    },
+//                    {
+//                        text: {
+//                            name: "فرع الخدمة",
+//                            link: '#13',
+//                        },
+//                    },
+//                    {
+//                        text: {
+//                            name: "فرع العديد ",
+//                            link: '#12',
+//                        },
+//                    }
+//                ]
+//            }
+//        ]
+//    }
+//};
